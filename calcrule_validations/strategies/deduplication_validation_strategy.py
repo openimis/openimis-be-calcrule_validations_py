cb4896878a40b1872948527d0239d3779d5833b9
sniedzielski <sniedzielski@soldevelo.com>
2023-12-29 16:38:13 +0100

    CM-382: incoming data dupllication detection

--- calcrule_validations/strategies/deduplication_validation_strategy.py
+++ calcrule_validations/strategies/deduplication_validation_strategy.py
@@ -8,6 +8,7 @@ class DeduplicationValidationStrategy(BaseValidationsStrategy):
     @classmethod
     def validate(cls, record, field_name, field_value, **kwargs) -> (object, bool, str):
         benefit_plan = kwargs.get('benefit_plan', None)
+        incoming_data = kwargs.get('incoming_data', None)
         # Query existing beneficiaries where is_deleted=False
         existing_beneficiaries = Beneficiary.objects.filter(benefit_plan__id=benefit_plan, is_deleted=False)
         # Check if the field value is duplicated among existing beneficiaries
@@ -15,12 +16,18 @@ class DeduplicationValidationStrategy(BaseValidationsStrategy):
             beneficiary.id for beneficiary in existing_beneficiaries
             if beneficiary.json_ext.get(f'{field_name}') == field_value
         ]
-        # Flag duplication in json_ext if duplicates are found
-        if duplicates:
+
+        # Check for duplication within incoming data
+        incoming_duplicates = incoming_data[incoming_data[field_name] == field_value].index.tolist() \
+            if incoming_data else []
+
+        # Flag duplication if duplicates are found
+        if duplicates or incoming_duplicates:
             record = {} if record is None else record
             record['duplication_notes'] = {
                 'duplicated': True,
-                'duplicated_id': duplicates
+                'duplicated_id': duplicates,
+                'incoming_duplicates': incoming_duplicates
             }
             return record, True, field_name, f"'{field_name}' Field value '{field_value}' is duplicated"
 
