3e36eed73cb9d72c63ca3fdede3356b14a4064a2
sniedzielski <sniedzielski@soldevelo.com>
2024-01-03 15:10:49 +0100

    CM-382: added incoming duplicates management

--- calcrule_validations/strategies/deduplication_validation_strategy.py
+++ calcrule_validations/strategies/deduplication_validation_strategy.py
@@ -16,20 +16,27 @@ class DeduplicationValidationStrategy(BaseValidationsStrategy):
         existing_beneficiaries = Beneficiary.objects.filter(benefit_plan__id=benefit_plan, is_deleted=False)
         # Check if the field value is duplicated among existing beneficiaries
         duplicates = [
-            beneficiary.id for beneficiary in existing_beneficiaries
+            {
+                'id': beneficiary.id,
+                'first_name': beneficiary.individual.first_name,
+                'last_name': beneficiary.individual.last_name,
+                'dob': beneficiary.individual.dob,
+                **beneficiary.json_ext  # Unpack all fields from json_ext
+            }
+            for beneficiary in existing_beneficiaries
             if beneficiary.json_ext.get(f'{field_name}') == field_value
         ]
 
         # Check for duplication within incoming data
-        incoming_duplicates = incoming_data[incoming_data[field_name] == field_value].index.tolist() \
-            if incoming_data else []
+        incoming_duplicates = incoming_data[incoming_data[field_name] == field_value].to_dict('records')
+        incoming_duplicates = [] if len(incoming_duplicates) <= 1 else incoming_duplicates
 
         # Flag duplication if duplicates are found
         duplications = None
         if duplicates or incoming_duplicates:
             duplications = {
                 'duplicated': True,
-                'duplicated_id': duplicates,
+                'duplicates_amoung_database': duplicates,
                 'incoming_duplicates': incoming_duplicates
             }
             return asdict(ValidationResult(
